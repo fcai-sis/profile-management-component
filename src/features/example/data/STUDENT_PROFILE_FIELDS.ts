9c7502b9add69c2c1bb48c6d1928a56fdbcfd8c3
Amr Khaled <lewolfje@gmail.com>
2024-03-03 18:30:01 +0200

    Add update profile fields

---
+++ src/features/example/data/STUDENT_PROFILE_FIELDS.ts
@@ -0,0 +1,5 @@
+export const STUDENT_PROFILE_FIELDS = ["phoneNumber", "address"];
+
+export const INSTRUCTOR_PROFILE_FIELDS = ["phoneNumber", "address", "officeHours"];
+
+export const TA_PROFILE_FIELDS = ["phoneNumber", "address", "officeHours"];
--- src/features/example/data/UPDATE_FIELDS.ts
+++
@@ -1,2 +0,0 @@
-export const UPDATE_FIELDS = ["phoneNumber", "address"];
-
