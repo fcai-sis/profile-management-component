d0e06ed8a4b3c7f75434beecfe1c2a10dd31c914
Amr Khaled <lewolfje@gmail.com>
2024-03-04 19:42:17 +0200

    Update student and instructor profile routes and middlewares

--- src/features/example/student-profile.routes.ts
+++ src/features/example/student-profile.routes.ts
@@ -3,20 +3,33 @@ import { Router } from "express";
 import { asyncHandler } from "@fcai-sis/shared-utilities";
 
 import updateStudentProfileHandler from "./logic/handlers/updateStudentProfile.handler";
-import updateStudentProfileValidator from "./logic/middlewares/validateUpdateFields.middleware";
-import ensureStudentIdInParamsMiddleware from "./logic/middlewares/ensureStudentIdInParams.middleware";
+import updateStudentProfileValidator from "./logic/middlewares/validateStudentUpdateFields.middleware";
+import ensureUserIdInParamsMiddleware from "./logic/middlewares/ensureUserIdInParams.middleware";
+import updateInstructorProfileValidator from "./logic/middlewares/validateInstructorUpdateFields.middleware";
+import updateInstructorProfileHandler from "./logic/handlers/updateInstructorProfile.handler";
 
 export default (router: Router) => {
   router.patch(
-    "/update-profile/:studentId",
+    "/update-student-profile/:userId",
 
     // Validate student ID
-    ensureStudentIdInParamsMiddleware,
+    ensureUserIdInParamsMiddleware,
 
     // Validate example message
     updateStudentProfileValidator,
 
     // Handle example request
     asyncHandler(updateStudentProfileHandler)
+  );
+
+  router.patch(
+    "/update-instructor-profile/:userId",
+    // Validate Instructor ID
+    ensureUserIdInParamsMiddleware,
+
+    // Validate profile fields
+    updateInstructorProfileValidator,
+
+    asyncHandler(updateInstructorProfileHandler)
   );
 };
