8f70e4d757992c609cfcdf473c1d45a9d0f69792
Amr Khaled <lewolfje@gmail.com>
2024-06-20 21:36:02 +0300

    add employee profile fields

--- src/features/profile/data/UPDATE_PROFILE_FIELDS.ts
+++ src/features/profile/data/UPDATE_PROFILE_FIELDS.ts
@@ -1,7 +1,12 @@
-
 // TODO : Add the fields that you want to update in the profile page
 export const STUDENT_PROFILE_FIELDS = ["phoneNumber", "address"];
 
-export const INSTRUCTOR_PROFILE_FIELDS = ["phoneNumber", "address", "officeHours"];
+export const INSTRUCTOR_PROFILE_FIELDS = [
+  "phoneNumber",
+  "address",
+  "officeHours",
+];
 
 export const TA_PROFILE_FIELDS = ["phoneNumber", "address", "officeHours"];
+
+export const EMPLOYEE_PROFILE_FIELDS = ["fullName", "email"];
