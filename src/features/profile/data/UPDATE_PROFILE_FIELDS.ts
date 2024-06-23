e5ffccf343da81186cc9808ace256b9e721f74c6
Amr Khaled <lewolfje@gmail.com>
2024-06-23 14:01:21 +0300

    Separate editable and immutable profile fields

--- src/features/profile/data/UPDATE_PROFILE_FIELDS.ts
+++ src/features/profile/data/UPDATE_PROFILE_FIELDS.ts
@@ -1,6 +1,20 @@
 // TODO : Add the fields that you want to update in the profile page
-export const STUDENT_PROFILE_FIELDS = ["fullName", "phoneNumber", "address"];
-
+export const EDITABLE_STUDENT_PROFILE_FIELDS = [
+  "fullName",
+  "phoneNumber",
+  "address",
+];
+export const IMMUTABLE_STUDENT_PROFILE_FIELDS = [
+  "studentId",
+  "scientificDivision",
+  "gender",
+  "religion",
+  "birthYear",
+  "birthMonth",
+  "birthDay",
+  "birthPlace",
+  "nationality",
+];
 export const INSTRUCTOR_PROFILE_FIELDS = [
   "phoneNumber",
   "address",
