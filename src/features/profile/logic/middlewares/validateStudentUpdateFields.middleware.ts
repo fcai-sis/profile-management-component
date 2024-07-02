59dbe5c0eddfa7175d025b0c09742381b9285e67
Amr Khaled <lewolfje@gmail.com>
2024-07-02 23:49:19 +0300

    sas

--- src/features/profile/logic/middlewares/validateStudentUpdateFields.middleware.ts
+++ src/features/profile/logic/middlewares/validateStudentUpdateFields.middleware.ts
@@ -1,46 +1,46 @@
-import { NextFunction, Request, Response } from "express";
-import { validationResult } from "express-validator";
-import { EDITABLE_STUDENT_PROFILE_FIELDS } from "../../data/UPDATE_PROFILE_FIELDS";
+// import { NextFunction, Request, Response } from "express";
+// import { validationResult } from "express-validator";
+// import { EDITABLE_STUDENT_PROFILE_FIELDS } from "../../data/UPDATE_PROFILE_FIELDS";
 
-import logger from "../../../../core/logger";
+// import logger from "../../../../core/logger";
 
-const updateStudentProfileValidator = [
-  (req: Request, res: Response, next: NextFunction) => {
-    logger.debug(
-      `Validating update student profile req body: ${JSON.stringify(req.body)}`
-    );
+// const updateStudentProfileValidator = [
+//   (req: Request, res: Response, next: NextFunction) => {
+//     logger.debug(
+//       `Validating update student profile req body: ${JSON.stringify(req.body)}`
+//     );
 
-    // if the request body contains any field other than those in UPDATE_FIELDS, return an error
-    const allowedFields = EDITABLE_STUDENT_PROFILE_FIELDS;
-    const receivedFields = Object.keys(req.body);
-    const invalidFields = receivedFields.filter(
-      (field) => !allowedFields.includes(field as any)
-    );
-    if (invalidFields.length > 0) {
-      logger.debug(
-        `Invalid req body provided ${JSON.stringify(invalidFields)}`
-      );
-      return res.status(400).json({
-        error: {
-          message: `Invalid fields provided: ${invalidFields}`,
-        },
-      });
-    }
+//     // if the request body contains any field other than those in UPDATE_FIELDS, return an error
+//     const allowedFields = EDITABLE_STUDENT_PROFILE_FIELDS;
+//     const receivedFields = Object.keys(req.body);
+//     const invalidFields = receivedFields.filter(
+//       (field) => !allowedFields.includes(field as any)
+//     );
+//     if (invalidFields.length > 0) {
+//       logger.debug(
+//         `Invalid req body provided ${JSON.stringify(invalidFields)}`
+//       );
+//       return res.status(400).json({
+//         error: {
+//           message: `Invalid fields provided: ${invalidFields}`,
+//         },
+//       });
+//     }
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      logger.debug(
-        `Invalid req body provided ${JSON.stringify(errors.array())}`
-      );
-      return res.status(400).json({
-        error: {
-          message: errors.array()[0].msg,
-        },
-      });
-    }
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       logger.debug(
+//         `Invalid req body provided ${JSON.stringify(errors.array())}`
+//       );
+//       return res.status(400).json({
+//         error: {
+//           message: errors.array()[0].msg,
+//         },
+//       });
+//     }
 
-    next();
-  },
-];
+//     next();
+//   },
+// ];
 
-export default updateStudentProfileValidator;
+// export default updateStudentProfileValidator;
